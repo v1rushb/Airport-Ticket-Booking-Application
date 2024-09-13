355540c67eeb4727ee7aa3ef8e6ce972b0ce5d5e
v1rushb <cs.bashar.herbawi@gmail.com>
2024-09-13 16:16:03 +0300

    Implementing Validations for Service Layers

--- Service/BookingService.cs
+++ Service/BookingService.cs
@@ -3,91 +3,18 @@ using Airport.Enums;
 using Airport.Interfaces;
 
 namespace Airport.Service {
-    // public class BookingService(FlightService flightService)
-    // {
-    //     private readonly List<Booking> _bookings = []; // TODO check why var here is not noice
-    //     private readonly FlightService _flightService = flightService;
-
-    //     public FlightService FlightService {
-    //         get {
-    //             return _flightService;
-    //         }
-    //     }
-
-    //     public void Book(Flight targetFlight, Passenger booker) {
-    //         _bookings.Add(new Booking { 
-    //             BID = Guid.NewGuid().ToString(),
-    //             flight = targetFlight,
-    //             passenger = booker,
-    //             BookingDate = DateTime.Now,
-    //             Price = targetFlight.Cost * GetPriceMultiplier(targetFlight.Class),
-    //         });
-    //         System.Console.WriteLine("Booked successfully!"); //remove later.
-    //     }
-
-    //     public List<Booking> GetAllBookings() {
-    //         return _bookings;
-    //     }
-
-    //     public Booking? GetBookingByID(string ID) {
-    //         return _bookings.FirstOrDefault(el => el.BID == ID);
-    //     }
-        // public static int GetPriceMultiplier(FlightClass flightClass) { // TODO: make a hashmap implemenation l8r
-        //     int ans = 1;
-        //     switch (flightClass)
-        //     {
-        //         case FlightClass.Economy:
-        //             ans = 1;
-        //             break;
-        //         case FlightClass.Business:
-        //             ans = 3;
-        //             break;
-        //         case FlightClass.FirstClass:
-        //             ans = 5;
-        //             break;
-        //     }
-        //     return ans;
-        // }
-
-    //     public bool CancelBooking(string ID) {
-    //         var booking = GetBookingByID(ID);
-    //         if(booking != null) {
-    //             _bookings.Remove(booking);
-    //             Console.WriteLine("Booking's been removed.");
-    //             return true;
-    //         }
-    //         Console.WriteLine("Booking wasn't found.");
-    //         return false;
-    //     }
-
-    //     public bool ModifyBooking(string ID, Flight newFlight) {
-    //         var booking = GetBookingByID(ID);
-    //         var targetFlight = _flightService.GetFlightByID(newFlight.FlightID);
-    //         if(targetFlight == null) { // probably too specific?
-    //             Console.WriteLine("Flight does not exist.");
-    //             return false;
-    //         }
-    //         if(booking != null) {
-    //             booking.flight = newFlight;
-    //             booking.Price = newFlight.Cost * GetPriceMultiplier(newFlight.Class);
-    //             Console.WriteLine($"Booking's been modified. current book: {booking}");
-    //             return true;
-    //         }
-    //         Console.WriteLine("Booking not found.");
-    //         return false;
-    //     }
-    // }
-
     public class BookingService : IBookingService {
         private readonly IRepository<Booking> _bookingRepository;
         private readonly IFlightService _flightService;
+        private readonly IValidationService _validateService;
 
-        public BookingService(IRepository<Booking> bookingRepository, IFlightService flightService) {
+        public BookingService(IRepository<Booking> bookingRepository, IFlightService flightService, IValidationService validationService) {
             _bookingRepository = bookingRepository;
             _flightService = flightService;
+            _validateService = validationService;
         }
 
-        public Booking BookFlight(Passenger bpassenger, Flight targetFlight) {
+        public Booking? BookFlight(Passenger bpassenger, Flight targetFlight) {
             var newBooking = new Booking {
                 BID = Guid.NewGuid().ToString(),
                 passenger = bpassenger,
@@ -95,9 +22,14 @@ namespace Airport.Service {
                 BookingDate = DateTime.Now,
                 Price = targetFlight.Cost * GetPriceMultiplier(targetFlight.Class)
             };
-            _bookingRepository.Add(newBooking);
-            return newBooking;
-            //very important: ADD VALIDATIONS!
+
+            var validationResult = _validateService.Validate(newBooking);
+            if(validationResult.IsValid) {
+                _bookingRepository.Add(newBooking);
+                return newBooking;
+            }
+            //check errors here.
+            return null;
         }
 
         public int GetPriceMultiplier(FlightClass flightClass) { // TODO: make a hashmap implemenation l8r
--- Service/FlightService.cs
+++ Service/FlightService.cs
@@ -2,71 +2,22 @@ using Airport.Utilties;
 using Airport.Models;
 using Airport.Enums;
 using Airport.Interfaces;
-using System.Security.Cryptography.X509Certificates;
-using System.Reflection.Metadata.Ecma335;
-using System.Buffers;
 
 namespace Airport.Service {
-    // public class FlightService {
-    //     private readonly List<Flight> _flights = new List<Flight>();
-
-    //     public void AddFlight(Flight flight) {
-    //         _flights.Add(flight);
-    //         System.Console.WriteLine("Flight's been added."); //remove later
-    //     }
-
-    //     public List<Flight> GetAllFlights() {
-    //         return _flights;
-    //     }
-
-    //     public Flight? GetFlightByID(string ID) {
-    //         return _flights.FirstOrDefault(el => el.FlightID == ID);
-    //     }
-
-    //     public void DisplayFlightInfo() {
-    //         foreach (var flight in _flights) {
-    //             System.Console.WriteLine($"Flight ID: {flight.FlightID}, From: {flight.DepartedCountry} to {flight.DestinatedCountry}, Class: {flight.Class}, Price: {flight.Cost}");
-    //         } // TODO: add DepartedAirport and ArrivedAirport
-    //     }
-
-    //     public void LoadFlights(string filePath) {
-    //         var flights = LoadFlightsFromCSV.LoadFlights(filePath);
-    //         _flights.AddRange(flights);
-
-    //         Console.WriteLine($"Loaded {flights.Count} into current flights.");
-    //     }
-
-    //     public void SaveFlights(string filePath) {
-    //         SaveFlightsIntoCSV.SaveFlights(filePath, _flights);
-
-    //         Console.WriteLine($"Saved {_flights.Count} into: {filePath}");
-    //     }
-
-    //     //TODO: implement the LookUpFlights() -> List<Flights> 
-    //     public List<Flight> LookUpFlights(string? departedCountry = null, string? destinatedCountry= null , DateTime? departureTime = null,
-    //                                         FlightClass? classType = null, string? departedAirport = null, string? arrivalAirport = null) { // method for a tpyical user.
-    //                                         return _flights.Where(el => (departedCountry== null || el.DepartedCountry.Equals(departedCountry, StringComparison.OrdinalIgnoreCase)) &&
-    //                                                                 (destinatedCountry == null || el.DestinatedCountry.Equals(destinatedCountry, StringComparison.OrdinalIgnoreCase)) &&
-    //                                                                 (departureTime == null || el.DepartureTime.Date == departureTime.Value.Date) &&
-    //                                                                 (classType == null || el.Class == classType)
-    //                                                                 && (departedAirport== null || el.DepartedAirport.Equals(departedAirport, StringComparison.OrdinalIgnoreCase)) 
-    //                                                                 && (arrivalAirport== null || el.ArrivalAirport.Equals(arrivalAirport, StringComparison.OrdinalIgnoreCase))).ToList();
-    //     }
-
-    //     //TODO make a flights have their own cap, making some unavailable.
-    // }
-
     public class FlightService : IFlightService {
         private readonly IRepository<Flight> _flightRepository;
         private readonly ICSVFlightLoader _csvFlightLoader;
         private readonly ICSVFlightSaver _csvFlightSaver;
 
+        private readonly IValidationService _validationService;
 
 
-        public FlightService(IRepository<Flight> flightRepository, ICSVFlightLoader csvFlightLoader, ICSVFlightSaver csvFlightSaver) {
+
+        public FlightService(IRepository<Flight> flightRepository, ICSVFlightLoader csvFlightLoader, ICSVFlightSaver csvFlightSaver, IValidationService validationService) {
             _flightRepository = flightRepository;
             _csvFlightLoader = csvFlightLoader;
             _csvFlightSaver = csvFlightSaver;
+            _validationService = validationService;
         }
 
         public IEnumerable<Flight> LookUpFlights(SearchCriteria criteria) {
@@ -82,9 +33,28 @@ namespace Airport.Service {
         
         public void LoadFlights(string targetCSVFilePath) {
             var flights = _csvFlightLoader.LoadFlights(targetCSVFilePath);
+            var validFlights = new List<Flight>();
+            var errors = new Dictionary<Flight, List<string>>();
 
-            foreach(var flight in flights) // Huge TODO: do a prof. validation.
+            foreach(var flight in flights) {
+                var validationResult = ValidateFlight(flight);
+                if(validationResult.IsValid) {
+                    validFlights.Add(flight);
+                } else {
+                    errors.Add(flight, validationResult.Errors.Select(el => el.ErrorMessage).ToList());
+                }
+            }
+            foreach(var flight in validFlights)
                 _flightRepository.Add(flight);
+
+            if(errors.Any()) {
+                foreach(var error in errors) {
+                    System.Console.WriteLine($"Errors for flight {error.Key.FlightID}: ");
+                    foreach(var msg in error.Value) {
+                        System.Console.WriteLine($"- {msg}");
+                    }
+                }
+            }
         }
 
         public void SaveFlights(string targetCSVFilePath) {
@@ -92,5 +62,12 @@ namespace Airport.Service {
             _csvFlightSaver.SaveFlights(targetCSVFilePath, flights);
         }
 
+        public ValidationResult ValidateFlight(Flight flight) {
+            return _validationService.Validate(flight);
+        }
+
+        public Dictionary<string, string> GetValidatedRules() {
+            return _validationService.GetValidationRules<Flight>();
+        }
     }
 }
